e61c29d0d22235500a537b488472fe0c5285c290
Marlon <marlonbuelvas314@gmail.com>
2025-05-08 13:34:33 -0500

    actualizado

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <contador></contador>
+  <ListaDeTarea></ListaDeTarea>
 </template>
 
 <script setup>
 import contador from './components/Contador.vue';
+import ListaDeTarea from './components/ListaDeTarea.vue';
 </script>
 
 <style scoped>
